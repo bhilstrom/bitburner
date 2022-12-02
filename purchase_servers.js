e8d5c2f4ea0a6c2cdf3ccc56080993ecb311e94f
bhilstrom <github@hilstrom.net>
2022-12-02 16:57:02 -0600

    Initial commit

---
+++ purchase_servers.js
@@ -0,0 +1,10 @@
+/** @param {import(".").NS } ns */
+export function main(ns) {
+
+    if (ns.args.length != 1)
+
+    ns.getPurchasedServers()
+    ns.getPurchasedServerLimit()
+
+    ns.weaken(ns.args[0])
+}
