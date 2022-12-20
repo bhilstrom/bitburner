e3bf8a625dbe10850c867f0800a07dca4b81b037
bhilstrom <github@hilstrom.net>
2022-12-20 12:44:40 -0600

    Add gang.common

---
+++ gang.common.js
@@ -0,0 +1,23 @@
+/* Gang task names
+[
+    "Unassigned",
+    "Mug People",
+    "Deal Drugs",
+    "Strongarm Civilians",
+    "Run a Con",
+    "Armed Robbery",
+    "Traffick Illegal Arms",
+    "Threaten & Blackmail",
+    "Human Trafficking",
+    "Terrorism",
+    "Vigilante Justice",
+    "Train Combat",
+    "Train Hacking",
+    "Train Charisma",
+    "Territory Warfare"
+]
+*/
+
+export function getTrainingTaskName(isHackGang) {
+    return isHackGang ? 'Train Hacking' : 'Train Combat'
+}
