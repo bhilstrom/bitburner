6944175ce28503f588cc79a5ec8efb31785747e4
bhilstrom <github@hilstrom.net>
2022-12-22 08:28:32 -0600

    Fix noisy logs, get max gang size

--- gang.recruit.js
+++ gang.recruit.js
@@ -1,15 +1,18 @@
 import { pp } from './common.js'
-import {getTrainingTaskName} from './gang.common'
+import { getMaxGangSize, getTrainingTaskName } from './gang.common'
 
 /** @param {import(".").NS } ns */
 export async function main(ns) {
+
+    [
+        "sleep",
+    ].forEach(logName => ns.disableLog(logName))
 
     const isHackGang = ns.gang.getGangInformation().isHacking
     const newRecruitTask = getTrainingTaskName(isHackGang)
 
     let members = ns.gang.getMemberNames()
-    while (members.length < 12) {
-        members = ns.gang.getMemberNames()
+    while (members.length < getMaxGangSize()) {
         if (ns.gang.canRecruitMember()) {
             const memberName = `Recruit-${members.length + 1}`
             ns.gang.recruitMember(memberName)
@@ -17,8 +20,9 @@ export async function main(ns) {
             ns.gang.setMemberTask(memberName, newRecruitTask)
         }
 
+        members = ns.gang.getMemberNames()
         await ns.sleep(15000) // 15 seconds
     }
 
-    pp(ns, 'All 12 gang members recruited!',  true)
+    pp(ns, `All ${getMaxGangSize} gang members recruited!`, true)
 }
