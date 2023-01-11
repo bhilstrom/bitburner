514dcd5cd730800307e44d4c1ed8f135c653eb39
Brian Hilstrom <hilstrom@google.com>
2023-01-11 09:06:00 -0600

    Create gang.start script, invoke from sleeve.murder

---
+++ gang.start.js
@@ -0,0 +1,55 @@
+import { pp } from './common.js'
+
+/** @param {import(".").NS } ns */
+function maybeStartGang(ns) {
+
+    if (ns.gang.inGang()) {
+        return
+    }
+
+    const availableCombatFactions = [
+        'The Syndicate',
+        'Slum Snakes',
+    ]
+
+    let factionForGang = undefined
+    for (let i = 0; i < availableCombatFactions.length; i++) {
+        const availableCombatFaction = availableCombatFactions[i]
+
+        if (ns.singularity.checkFactionInvitations().includes(availableCombatFaction)) {
+            ns.singularity.joinFaction(availableCombatFaction)
+            factionForGang = availableCombatFaction
+            break
+        }
+
+        if (ns.getPlayer().factions.includes(availableCombatFaction)) {
+            factionForGang = availableCombatFaction
+            break
+        }
+    }
+
+    if (factionForGang === undefined) {
+        throw new Error('Unable to find a combat faction to create a gang.')
+    }
+
+    ns.gang.createGang(factionForGang)
+}
+
+/** @param {import(".").NS } ns */
+export async function main(ns) {
+    pp(ns, 'Starting gang management', true)
+
+    maybeStartGang(ns)
+
+    const scripts = [
+        'gang.recruit.js',
+        'gang.ascend.js',
+        'gang.grow.js',
+    ]
+
+    scripts.forEach(script => {
+        if (!ns.exec(script, 'home')) {
+            throw new Error(`Failed to start gang script ${script}`)
+        }
+    })
+}
--- sleeve.murder.js
+++ sleeve.murder.js
@@ -100,44 +100,5 @@ export async function main(ns) {
         ns.sleeve.setToShockRecovery(sleeveNum)
     })
 
-    pp(ns, 'Starting gang management', true)
-
-    const availableCombatFactions = [
-        'The Syndicate',
-        'Slum Snakes',
-    ]
-
-    let factionForGang = undefined
-    for (let i = 0; i < availableCombatFactions.length; i++) {
-        const availableCombatFaction = availableCombatFactions[i]
-
-        if (ns.singularity.checkFactionInvitations().includes(availableCombatFaction)) {
-            ns.singularity.joinFaction(availableCombatFaction)
-            factionForGang = availableCombatFaction
-            break
-        }
-
-        if (ns.getPlayer().factions.includes(availableCombatFaction)) {
-            factionForGang = availableCombatFaction
-            break
-        }
-    }
-
-    if (factionForGang === undefined) {
-        throw new Error('Unable to find a combat faction to create a gang.')
-    }
-
-    ns.gang.createGang(factionForGang)
-
-    const scripts = [
-        'gang.recruit.js',
-        'gang.ascend.js',
-        'gang.grow.js',
-    ]
-
-    scripts.forEach(script => {
-        if (!ns.exec(script, 'home')) {
-            throw new Error(`Failed to start gang script ${script}`)
-        }
-    })
+    ns.spawn('gang.start.js', 1)
 }
