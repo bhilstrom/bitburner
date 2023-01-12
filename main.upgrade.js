ad474e0d422ba86f9c3682cb5a8d41bc005a64e8
Brian Hilstrom <hilstrom@google.com>
2023-01-12 09:43:46 -0600

    Upgrade until 32TB

--- main.upgrade.js
+++ main.upgrade.js
@@ -8,9 +8,12 @@ export async function main(ns) {
     ].forEach(logName => ns.disableLog(logName))
 
     let home = ns.getServer('home')
-    while (home.maxRam < 1024) {
+    // 2^10 is 1024 GB (1 TB)
+    // 2^15 is 32 TB
+    const desiredRam = Math.pow(2, 15)
+    while (home.maxRam < desiredRam) {
 
-        pp(ns, `Max home ram is ${home.maxRam}, trying to upgrade...`)
+        pp(ns, `Max home ram is ${home.maxRam}, trying to upgrade until ${desiredRam}...`)
         ns.singularity.upgradeHomeRam()
 
         await ns.sleep(60 * 1000)
