213c26e2e69adca222f0e94d4aa0af20984cdb83
Brian Hilstrom <hilstrom@google.com>
2023-01-15 22:34:52 -0600

    Upgrade home, buy sleeve augs, fail if not enough augs

--- ascend.js
+++ ascend.js
@@ -1,4 +1,7 @@
-import { pp } from './common.js'
+import { forEachSleeve, pp } from './common.js'
+
+const FORCE_HACK = 'forceHack'
+const FORCE_NUMBER = 'forceNumber'
 
 const HACKING_STATS = [
     'hacking',
@@ -83,6 +86,7 @@ function getAugToPurchase(ns, desiredStats) {
 
 /** @param {import(".").NS } ns */
 async function purchaseStatAugs(ns, statsToPurchase) {
+    let augsPurchasedCount = 0
     let augToPurchase = getAugToPurchase(ns, statsToPurchase)
     while (augToPurchase) {
         const faction = augToPurchase.factions[0]
@@ -91,22 +95,15 @@ async function purchaseStatAugs(ns, statsToPurchase) {
         if (!ns.singularity.purchaseAugmentation(faction, name)) {
             throw new Error(`Failed to purchase ${name}`)
         }
+        augsPurchasedCount += 1
         await ns.sleep(100)
         augToPurchase = getAugToPurchase(ns, statsToPurchase)
     }
+    return augsPurchasedCount
 }
 
 /** @param {import(".").NS } ns */
-export async function main(ns) {
-    /*
-    - Purchase any +hack augs
-    - Purchase any +faction rep augs
-    - Purchase as many NeuroFlux as we can
-        - Donate to faction to increase rep if necessary
-    */
-    await purchaseStatAugs(ns, HACKING_STATS)
-    await purchaseStatAugs(ns, FACTION_STATS)
-
+function purchaseNeuroFluxGovernors(ns) {
     let gangFaction = undefined
     if (ns.gang.inGang()) {
         gangFaction = ns.gang.getGangInformation().faction
@@ -129,5 +126,81 @@ export async function main(ns) {
     })
 
     // Purchase all available NeuroFlux Governor
-    while (ns.singularity.purchaseAugmentation(maxRepFaction, 'NeuroFlux Governor')) {}
+    while (ns.singularity.purchaseAugmentation(maxRepFaction, 'NeuroFlux Governor')) { }
+}
+
+/** @param {import(".").NS } ns */
+async function purchaseSleeveAugs(ns) {
+    forEachSleeve(ns, (sleeveNum) => {
+        let augCount = 0
+        ns.sleeve.getSleevePurchasableAugs(sleeveNum).forEach(augName => {
+            if (ns.sleeve.purchaseSleeveAug(sleeveNum, augName)) {
+                augCount += 1
+            }
+        })
+
+        if (augCount > 0) {
+            pp(ns, `Purchased ${augCount} augs for sleeve ${sleeveNum}`, true)
+        }
+    })
+}
+
+/** @param {import(".").NS } ns */
+async function upgradeHomeMachine(ns) {
+    let homeServer = ns.getServer('home')
+
+    // Only upgrade to 256 TB
+    while (homeServer.maxRam < Math.pow(2, 18) && ns.singularity.upgradeHomeRam()) {
+        pp(ns, `Home machine RAM upgraded!`, true)
+        await ns.sleep(100)
+        homeServer = ns.getServer('home')
+    }
+
+    // Only upgrade to 5 cores
+    while (homeServer.cpuCores < 5 && ns.singularity.upgradeHomeCores()) {
+        pp(ns, `Home machine cores upgraded!`, true)
+        await ns.sleep(100)
+        homeServer = ns.getServer('home')
+    }
+}
+
+/** @param {import(".").NS } ns */
+export async function main(ns) {
+
+    const forceHack = ns.args.includes(FORCE_HACK)
+    const forceNumber = ns.args.includes(FORCE_NUMBER)
+
+    /*
+    - Purchase any +hack augs
+    - Purchase any +faction rep augs
+    - Purchase as many NeuroFlux as we can
+        - Donate to faction to increase rep if necessary
+    */
+    await upgradeHomeMachine(ns)
+
+    const hackingAugCount = await purchaseStatAugs(ns, HACKING_STATS)
+    if (hackingAugCount < 4 && !forceHack) {
+        pp(ns, `Only able to purchase ${hackingAugCount} hacking augs, stopping script. Run with '${FORCE_HACK}' to bypass check.`, true)
+        return
+    }
+
+    await purchaseStatAugs(ns, FACTION_STATS)
+    
+    purchaseNeuroFluxGovernors()
+
+    // Number of augs we're purchasing is 'owned(true)' - 'owned(false)'
+    const numberOfAugsPending = ns.singularity.getOwnedAugmentations(true).length - ns.singularity.getOwnedAugmentations(false).length
+    if (numberOfAugsPending < 10 && !forceNumber) {
+        pp(ns, `Only ${numberOfAugsPending} augs are pending installation, stopping script. Run with '${FORCE_NUMBER} to bypass check.`, true)
+        return
+    }
+
+    await purchaseSleeveAugs(ns)
+}
+
+export function autocomplete(data, args) {
+    return [
+        FORCE_HACK,
+        FORCE_NUMBER,
+    ]
 }
