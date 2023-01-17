0a90d993eedee3fb074881a86cd968d329b3d028
Brian Hilstrom <hilstrom@google.com>
2023-01-16 20:26:47 -0600

    Maybe fix runAndWaitFor v2

--- common.js
+++ common.js
@@ -24,8 +24,8 @@ export function settings() {
 }
 
 /** @param {import(".").NS } ns */
-export async function runAndWaitFor(ns, scriptName, args) {
-  if (!ns.exec(scriptName, 'home', 1, args || [])) {
+export async function runAndWaitFor(ns, scriptName, ...args) {
+  if (!ns.exec(scriptName, 'home', 1, args)) {
     throw new Error(`Failed to start ${scriptName}`)
   }
 
