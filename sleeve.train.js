bfc43a6bd461d4a6c271624d1a85584d17c32be1
bhilstrom <github@hilstrom.net>
2023-01-23 08:11:58 -0600

    Export functions

--- sleeve.train.js
+++ sleeve.train.js
@@ -5,18 +5,19 @@ function trainHacking(ns, sleeveNumber) {
     ns.sleeve.setToUniversityCourse(sleeveNumber, ns.enums.LocationName.Sector12RothmanUniversity, 'Study Computer Science')
 }
 
-
-/** @param {import(".").NS } ns */
-function workForFaction(ns, sleeveNumber, faction) {
-
-    // Over time, sleeves will greatly exceed our skills in security work.
+function getDefaultWorkTypes() {
+    // Over time, sleeves will greatly exceed our skills in security work due to augs.
     // We prioritize field work over security just because it also helps our hacking.
     // Hacking is a last resort, as sleeves are never very good at it.
-    const workTypes = [
+    return [
         ns.enums.FactionWorkType.field,
         ns.enums.FactionWorkType.security,
         ns.enums.FactionWorkType.hacking,
     ]
+}
+
+/** @param {import(".").NS } ns */
+function workForFaction(ns, sleeveNumber, faction, workTypes) {
 
     for (let i = 0; i < workTypes.length; i++) {
         const workType = workTypes[i]
@@ -48,6 +49,51 @@ function getPriorityFactions() {
     ]
 }
 
+export function assignAllSleeves(ns, workTypes = undefined) {
+    const workTypes = workTypes || getDefaultWorkTypes()
+
+    const priorityFactions = getPriorityFactions()
+    let factions = ns.getPlayer().factions
+
+    if (ns.gang.inGang()) {
+        const gangFaction = ns.gang.getGangInformation().faction
+        factions = factions.filter(faction => gangFaction != faction)
+    }
+
+    forEachSleeve(ns, (sleeveNum) => {
+        const sleeve = ns.sleeve.getSleeve(sleeveNum)
+        if (sleeve.shock > 5) {
+            pp(ns, `Sleeve ${sleeveNum} has shock ${sleeve.shock}, recovering`)
+            ns.sleeve.setToShockRecovery(sleeveNum)
+        } else {
+
+            // Work for the first available priority faction, if we can.
+            let faction = priorityFactions.shift()
+            while (faction && !factions.includes(faction)) {
+                faction = priorityFactions.shift()
+            }
+
+            // If we don't have any priority factions, just work for the first one.
+            if (!faction || !factions.includes(faction)) {
+                faction = factions.shift()
+            }
+
+            // If we don't have ANY faction to work for, Mug people.
+            if (!faction) {
+                ns.sleeve.setToCommitCrime(sleeveNum, ns.enums.CrimeType.mug)
+                return
+            }
+
+            workForFaction(ns, sleeveNum, faction, workTypes) 
+            
+            // Remove the faction from the list.
+            // We don't care if the work assignment was successful or not,
+            // because any errors will get retried in the next loop.
+            factions = factions.filter(option => option != faction)
+        }
+    })
+}
+
 /** @param {import(".").NS } ns */
 export async function main(ns) {
 
@@ -63,46 +109,7 @@ export async function main(ns) {
     }
 
     while (true) {
-        const priorityFactions = getPriorityFactions()
-        let factions = ns.getPlayer().factions
-
-        if (ns.gang.inGang()) {
-            const gangFaction = ns.gang.getGangInformation().faction
-            factions = factions.filter(faction => gangFaction != faction)
-        }
-
-        forEachSleeve(ns, (sleeveNum) => {
-
-            const sleeve = ns.sleeve.getSleeve(sleeveNum)
-            if (sleeve.shock > 5) {
-                pp(ns, `Sleeve ${sleeveNum} has shock ${sleeve.shock}, recovering`)
-                ns.sleeve.setToShockRecovery(sleeveNum)
-            } else {
-
-                // Work for the first available priority faction, if we can.
-                let faction = priorityFactions.shift()
-                while (faction && !factions.includes(faction)) {
-                    faction = priorityFactions.shift()
-                }
-
-                // If we don't have any priority factions, just work for the first one.
-                if (!faction || !factions.includes(faction)) {
-                    faction = factions.shift()
-                }
-
-                // If we don't have ANY faction to work for, Mug people.
-                if (!faction) {
-                    ns.sleeve.setToCommitCrime(sleeveNum, ns.enums.CrimeType.mug)
-                }
-
-                // If we successfully worked for the faction,
-                // remove it from the options for the rest of the sleeves.
-                workForFaction(ns, sleeveNum, faction) 
-                
-                // We don't care if the work assignment was successful or not.
-                factions = factions.filter(option => option != faction)
-            }
-        })
+        assignAllSleeves(ns)
 
         await ns.sleep(30 * 1000)
     }
